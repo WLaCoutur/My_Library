7aab6382ad3687021f22eda43078e6847b08c58a
# install nbextensions <jupyter contrib nbextension install --user>
2025-05-29 12:15:32 +0200

    Add Re_search(Domain)

---
+++ Re_search(Domain).py
@@ -0,0 +1,12 @@
+import pythoin3
+import re
+def check_web_address(text):
+  pattern = r"^[a-zA-Z0-9_.-]+\.[a-zA-Z]+$"
+  result = re.search(pattern, text)
+  return result != None
+
+print(check_web_address("gmail.com")) # True
+print(check_web_address("www@google")) # False
+print(check_web_address("www.Coursera.org")) # True
+print(check_web_address("web-address.com/homepage")) # False
+print(check_web_address("My_Favorite-Blog.US")) # True
